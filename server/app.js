bb4ce323e1897c6b5669c64772c1cafd5b183833
Hammed Noibi <hnobi08@yahoo.com>
2017-11-22 03:56:45 -1100

    create babelrc

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+const port = parseInt((process.env.PORT), 10) || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+    res.status(404)
+        .json({
+            message: 'Welcome Message ,am working perfectly',
+        });
+});
+
+
+// app.use('/api', recipeRoutes)
+
+app.listen(port, console.log(`server running at ${port}`));
+
+export default app;
