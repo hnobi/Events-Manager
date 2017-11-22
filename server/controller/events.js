50bcbfcd40c439868cc58a14785f46b74d97ffd2
Hammed Noibi <hnobi08@yahoo.com>
2017-11-22 12:21:40 -1100

    create modifyEvents controller

--- server/controller/events.js
+++ server/controller/events.js
@@ -2,6 +2,14 @@
 import eventsData from '../models/events';
 
 export default class EventsController {
+    /**
+               * add event to the existing events
+               * @param {obj} req
+               * @param {obj} res
+               * @memberof EventsController
+               * @return {obj} insertion error messages or success messages
+               */
+
     static addevent(req, res) {
         const {
       title, location, date, description,
@@ -17,8 +25,44 @@ export default class EventsController {
         res.status(201);
         res.json({
             status: 'Success',
-            message: 'Successfully added new recipe',
+            message: 'Successfully added new Event',
             eventsData,
+        });
+    }
+
+
+    /**
+     * modified or update existing event
+     * @static
+     * @param {obj} req 
+     * @param {obj} res 
+     *  @return {obj} insertion error messages or success messages
+     * @memberof EventsController
+     */
+    static modifyEvent(req, res) {
+        const {
+      title, location, date, description,
+    } = req.body;
+        for (let i = 0; i < eventsData.length; i += 1) {
+            if (eventsData[i].id === parseInt(req.params.eventsID, 10)) {
+                eventsData[i].title = (title) || eventsData[i].title;
+                eventsData[i].location = (location) || eventsData[i].location;
+                eventsData[i].description = (description) || eventsData[i].description;
+                eventsData[i].date = (date) || eventsData[i].date;
+                res.status(200)
+                    .json({
+                        status: 'Success',
+                        message: 'Successfully updated event',
+                        eventsData,
+                    });
+            }
+
+
+        }
+        res.status(400);
+        res.json({
+            status: 'Failed',
+            message: 'event id does not exist',
         });
     }
 }
