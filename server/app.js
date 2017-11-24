517f6b7dc7e436c27b81b6ac5209701e8dedaa86
Hammed Noibi <hnobi08@yahoo.com>
2017-11-24 04:43:05 -1100

    update eslint

--- server/app.js
+++ server/app.js
@@ -12,27 +12,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/', (req, res) => {
-    res.status(404)
-        .json({
-            message: 'Welcome to EventsManager',
-        });
+  res.status(404)
+    .json({
+      message: 'Welcome to EventsManager',
+    });
 });
 
 app.use('/api/v1', eventsRoute);
 app.use('/api/v1/', (req, res) => {
-    res.status(404);
-    res.json({
-        status: 'Failed',
-        message: 'Page not found'
-    });
+  res.status(404);
+  res.json({
+    status: 'Failed',
+    message: 'Page not found'
+  });
 });
 
 app.use('*', (req, res) => {
-    res.status(404);
-    res.json({
-        status: 'Failed',
-        message: 'Page not found'
-    });
+  res.status(404);
+  res.json({
+    status: 'Failed',
+    message: 'Page not found'
+  });
 });
 
 app.listen(port, () => console.log(`Application listening  on port ${port}`));
