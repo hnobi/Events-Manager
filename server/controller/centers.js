fe2f924be17b212a2c1041187c295ffd5ffd47d2
Hammed Noibi <hnobi08@yahoo.com>
2017-11-23 20:39:15 -1100

    make getsinglecenter better

--- server/controller/centers.js
+++ server/controller/centers.js
@@ -35,14 +35,13 @@ export default class CentersController {
                                     * @return {obj} insertion error messages or success messages
                                     */
   static getSingleCenter(req, res) {
-    for (let j = 0; j < centersData.length; j += 1) {
-      if (centersData[j].id === parseInt(req.params.centerId, 10){
-        return centersData[j];
+    for (let j = 0; j < centersData.length; j++) {
+      if (centersData[j].id === parseInt(req.params.centerId, 10)) {
         res.status(200)
           .json({
             status: 'Success',
-            message: 'Successfully retrived a center',
-            // centersData,
+            message: centersData[j],
+
           });
       }
     }
@@ -52,44 +51,39 @@ export default class CentersController {
         message: 'center  id does not exist',
       });
   }
-    return res.status(400)
-  .json({
-    status: 'failed',
-    message: 'center  id does not exist',
-  });
-  }
 
-    /**
-                  * modified or update existing center
-                  * @static
-                  * @param {obj} req
-                  * @param {obj} res
-                  *  @return {obj} insertion error messages or success messages
-                  * @memberof CenterController
-                  */
-    static modifycenter(req, res) {
-  const {
+
+  /**
+                * modified or update existing center
+                * @static
+                * @param {obj} req
+                * @param {obj} res
+                *  @return {obj} insertion error messages or success messages
+                * @memberof CenterController
+                */
+  static modifycenter(req, res) {
+    const {
       name, address, facilities,
     } = req.body;
-  for (let i = 0; i < centersData.length; i += 1) {
-    if (centersData[i].id === parseInt(req.params.centerId, 10)) {
-      centersData[i].name = (name) || centersData[i].name;
-      centersData[i].address = (address) || centersData[i].address;
-      centersData[i].facilities = (facilities) || centersData[i].facilities;
-      res.status(200)
-        .json({
-          status: 'Success',
-          message: 'Successfully updated center',
-          centersData,
-        });
+    for (let i = 0; i < centersData.length; i += 1) {
+      if (centersData[i].id === parseInt(req.params.centerId, 10)) {
+        centersData[i].name = (name) || centersData[i].name;
+        centersData[i].address = (address) || centersData[i].address;
+        centersData[i].facilities = (facilities) || centersData[i].facilities;
+        res.status(200)
+          .json({
+            status: 'Success',
+            message: 'Successfully updated center',
+            centersData,
+          });
+      }
     }
+    res.status(400);
+    res.json({
+      status: 'Failed',
+      message: 'center id does not exist',
+    });
   }
-  res.status(400);
-  res.json({
-    status: 'Failed',
-    message: 'center id does not exist',
-  });
-}
 
 
 
@@ -101,11 +95,11 @@ export default class CentersController {
                                         * @return {obj} insertion error messages or success messages
                                         */
   static showAllCenters(req, res) {
-  if (centersData.length !== 0) {
-    res.status(200)
-      .json(centersData);
+    if (centersData.length !== 0) {
+      res.status(200)
+        .json(centersData);
+    }
+    return res.status(400).json({ message: 'No available recipes' });
   }
-  return res.status(400).json({ message: 'No available recipes' });
-}
 }
 
